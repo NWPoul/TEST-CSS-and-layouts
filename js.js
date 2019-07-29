227a465466974528c0d67472d597357480fa1853
Pavel <nwpoul@yandex.ru>
2019-07-30 01:26:08 +0300

    cmt

--- js.js
+++ js.js
@@ -1,42 +1,72 @@
+/*
+test note
+*/
 
 function start(params) {
-  let table = document.getElementById('mainTable');
+  var table = document.getElementById('mainTable');
   doTable(table);
 
-  let targetElementId = 'day_3_Tr';
-  
-  
+  var targetElementId = 'day_3_Tr';
+
   testObserver(targetElementId);
 }
 
 
 function testObserver(targetElementId) {
-    //console.log('setObserver');
-    
-    var options = {
-      root: document.getElementById('inner'),
-      rootMargin: '-15% 0% 0% 0%'      
-    }
-    var callback = function(entries, observer) {
-        entries.forEach(entry => {
-            var subj = {
-            //time : entry.time,               // a DOMHightResTimeStamp indicating when the intersection occurred.
-            roorBounds : entry.rootBounds,         // a DOMRectReadOnly for the intersection observer's root.
-            bClRect : entry.boundingClientRect, // a DOMRectReadOnly for the intersection observer's target.
-            intersectionRext : entry.intersectionRect,   // a DOMRectReadOnly for the visible portion of the intersection observer's target.
-            intsRatio : entry.intersectionRatio,  // the number for the ratio of the intersectionRect to the boundingClientRect.
-            target : entry.target,             // the Element whose intersection with the intersection root changed.
-            isIntersecting : entry.isIntersecting     // intersecting: true or false
-            }
-            console.log(subj);
-        });
-    };
+  //console.log('setObserver');
 
-    var observer = new IntersectionObserver(callback, options);
-    var target = document.getElementById(targetElementId);
-    target.classList.add('target')
+  var options = {
+    root: document.getElementById('inner'),
+    rootMargin: '-15% 0% 0% 0%'
+  };
+  var callback = function(entries, observer) {
+    entries.forEach(entry => {
+      var subj = {
+        //time : entry.time,               // a DOMHightResTimeStamp indicating when the intersection occurred.
+        roorBounds : entry.rootBounds,         // a DOMRectReadOnly for the intersection observer's root.
+        bClRect : entry.boundingClientRect, // a DOMRectReadOnly for the intersection observer's target.
+        intersectionRext : entry.intersectionRect,   // a DOMRectReadOnly for the visible portion of the intersection observer's target.
+        intsRatio : entry.intersectionRatio,  // the number for the ratio of the intersectionRect to the boundingClientRect.
+        target : entry.target,             // the Element whose intersection with the intersection root changed.
+        isIntersecting : entry.isIntersecting     // intersecting: true or false
+      };
+      //console.log(subj);
+      changeHeader();
+    });
+  };
 
-    observer.observe(target);
+  var observer = new IntersectionObserver(callback, options);
+  var target = document.getElementById(targetElementId);
+  target.classList.add('target');
+
+  observer.observe(target);
+}// end testObserver
+
+function changeHeader(params) {
+  let anchor = document.getElementById('r0c0');
+  let anchorRect = anchor.getBoundingClientRect();
+  let xPos = anchorRect.left+10;
+  let yPos = anchorRect.bottom+10;
+
+  let checkElem = document.elementFromPoint(xPos, yPos);
+  console.log('yPos=' +yPos, 'elem: ', checkElem.innerHTML);
+
+  var newEl = document.createElement('div');
+  newEl.innerHTML = '$';
+  newEl.style.cssText = 'position:fixed;'
+  +'left:' +xPos +'px;'
+  +'top:' +(yPos + 1) +'px;'
+  +'z-index:100;'
+  +'border: 1px solid #000;'
+  +'background-color: red';
+
+  document.body.appendChild(newEl);
+
+  setTimeout(function() {
+    document.body.removeChild(newEl);
+  }, 3000);
+
+
 }
 
 
@@ -48,32 +78,32 @@ function doTable(table) {
   let daysN = 5;
 
   table.appendChild( doHeadTbody(colsCnt) );
-  
+
   for (let index = 1; index <= daysN; index++) {
     table.appendChild( doTbody(rowsCnt, 0, index, '') );
-  };
-  
+  }
 
-  return table
+
+  return table;
 
   function doHeadTbody(colsCnt) {
     let headTbody = document.createElement('tbody');
-    headTbody.id = "btHeaderTbody";
+    headTbody.id = 'btHeaderTbody';
     headTbody.classList.add('bookingTbody');
 
     let hTr =  document.createElement('tr');
-    hTr.id = "btHeaderTr";
+    hTr.id = 'btHeaderTr';
     hTr.classList.add('tableHeader');
 
     let hTh =  document.createElement('th');
-    hTh.id = "r0c0";
+    hTh.id = 'r0c0';
     hTh.classList.add('th0');
-    hTh.innerHTML = 'th0'
+    hTh.innerHTML = 'th0';
 
     let divHeader =  document.createElement('div');
-    divHeader.id = "divHeader";
+    divHeader.id = 'divHeader';
     divHeader.classList.add('divHeader');
-    divHeader.innerHTML = 'div Header!'
+    divHeader.innerHTML = 'div Header!';
 
     hTr.appendChild(hTh);
     for (let index = 0; index < colsCnt; index++) {
@@ -105,7 +135,7 @@ function doTable(table) {
     tbody.appendChild(hTr);
 
     for (let index = 0; index < rowsCnt; index++) {
-      let className = (index % 2 == 0) ? 'groupD' : 'groupD odd'
+      let className = (index % 2 == 0) ? 'groupD' : 'groupD odd';
       let rowsStr = `<tr id="day${dayN}_${index}" class="${className}">
       <th rowspan="2">${index} <br><span class="tdSpan"></span></th>
       <td class="pf">v1</td>
@@ -114,17 +144,17 @@ function doTable(table) {
     </tr>
     <tr class="groupN">
       <td colspan="3" class="freeTimeTd"> -- ft marker --</td>
-    </tr>`
-     
-    tbody.insertAdjacentHTML("beforeend", rowsStr);
-    }// end for rowsCnt  
-      
-      // let tr =  document.createElement('tr');
-      // tr.id = dayN +'_' +'slot-' +index;
-      // tr.classList.add('groupD');
-      // if(index % 2 == 0) { tr.classList.add(odd) }
-      // tr.innerHTML = 
-    
+    </tr>`;
+
+      tbody.insertAdjacentHTML('beforeend', rowsStr);
+    }// end for rowsCnt
+
+    // let tr =  document.createElement('tr');
+    // tr.id = dayN +'_' +'slot-' +index;
+    // tr.classList.add('groupD');
+    // if(index % 2 == 0) { tr.classList.add(odd) }
+    // tr.innerHTML =
+
     return tbody;
   }// end do Tbody
 }// end do table
