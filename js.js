3631fa6b75ad97a41aee3d96b1e51c4c12a85b24
nwpoul <nwpoul@yandex.ru>
2019-07-29 20:26:38 +0300

    ...

--- js.js
+++ js.js
@@ -1,14 +1,23 @@
-function testObserver() {
-    console.log('setObserver');
 
+function start(params) {
+  let table = document.getElementById('mainTable');
+  doTable(table);
+
+  let targetElementId = 'day_3_Tr';
+  
+  
+  testObserver(targetElementId);
+}
+
+
+function testObserver(targetElementId) {
+    //console.log('setObserver');
+    
     var options = {
       root: document.getElementById('inner'),
-    //   rootMargin: '20% 0% 0% 50%'      
+      rootMargin: '-15% 0% 0% 0%'      
     }
     var callback = function(entries, observer) {
-   
-      console.log(observer.root);
-   
         entries.forEach(entry => {
             var subj = {
             //time : entry.time,               // a DOMHightResTimeStamp indicating when the intersection occurred.
@@ -24,7 +33,98 @@ function testObserver() {
     };
 
     var observer = new IntersectionObserver(callback, options);
-    var target = document.getElementById('day2HeaderTh');
+    var target = document.getElementById(targetElementId);
+    target.classList.add('target')
 
     observer.observe(target);
 }
+
+
+
+
+function doTable(table) {
+  let colsCnt = 3;
+  let rowsCnt = 10;
+  let daysN = 5;
+
+  table.appendChild( doHeadTbody(colsCnt) );
+  
+  for (let index = 1; index <= daysN; index++) {
+    table.appendChild( doTbody(rowsCnt, 0, index, '') );
+  };
+  
+
+  return table
+
+  function doHeadTbody(colsCnt) {
+    let headTbody = document.createElement('tbody');
+    headTbody.id = "btHeaderTbody";
+    headTbody.classList.add('bookingTbody');
+
+    let hTr =  document.createElement('tr');
+    hTr.id = "btHeaderTr";
+    hTr.classList.add('tableHeader');
+
+    let hTh =  document.createElement('th');
+    hTh.id = "r0c0";
+    hTh.classList.add('th0');
+    hTh.innerHTML = 'th0'
+
+    let divHeader =  document.createElement('div');
+    divHeader.id = "divHeader";
+    divHeader.classList.add('divHeader');
+    divHeader.innerHTML = 'div Header!'
+
+    hTr.appendChild(hTh);
+    for (let index = 0; index < colsCnt; index++) {
+      let hTd =  document.createElement('td');
+      //hTd.id = "hTd";
+      hTd.classList.add('td-header');
+      hTd.innerHTML = 'td' +index;
+
+      hTr.appendChild(hTd);
+    }//end for cols
+
+    headTbody.appendChild(hTr);
+
+    return headTbody;
+  }// end do doHeadTbody
+
+
+  function doTbody(rowsCnt, colsCnt, dayN, dayHeader) {
+    let tbody = document.createElement('tbody');
+    tbody.id = dayN;
+    tbody.classList.add('bookingTbody');
+
+    let hTr =  document.createElement('tr');
+    hTr.id = 'day_' +dayN +'_Tr';
+    hTr.classList.add('newDayTr');
+    hTr.innerHTML = `<th>day_${dayN}</th>
+                     <td colspan="3" class="newDayTr">full_day_${dayN}</td>`;
+
+    tbody.appendChild(hTr);
+
+    for (let index = 0; index < rowsCnt; index++) {
+      let className = (index % 2 == 0) ? 'groupD' : 'groupD odd'
+      let rowsStr = `<tr id="day${dayN}_${index}" class="${className}">
+      <th rowspan="2">${index} <br><span class="tdSpan"></span></th>
+      <td class="pf">v1</td>
+      <td class="pf">v2</td>
+      <td class="pf">v3</td>
+    </tr>
+    <tr class="groupN">
+      <td colspan="3" class="freeTimeTd"> -- ft marker --</td>
+    </tr>`
+     
+    tbody.insertAdjacentHTML("beforeend", rowsStr);
+    }// end for rowsCnt  
+      
+      // let tr =  document.createElement('tr');
+      // tr.id = dayN +'_' +'slot-' +index;
+      // tr.classList.add('groupD');
+      // if(index % 2 == 0) { tr.classList.add(odd) }
+      // tr.innerHTML = 
+    
+    return tbody;
+  }// end do Tbody
+}// end do table
