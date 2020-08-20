3e01ce4f6233ddf8b45de56f91f96655a1ddffd9
Pavel <nwpoul@yandex.ru>
2020-08-20 15:29:37 +0300

    testModule

---
+++ JS/Modules/m1.js
@@ -0,0 +1,6 @@
+export default {
+    testMF: (msg = 'defMSG')=>{
+        console.log(msg);
+        return msg;
+    }
+};
---
+++ JS/main.js
@@ -0,0 +1,9 @@
+import m1 from "./Modules/m1.js";
+
+function testStart() {
+    let testSpan = document.getElementById('testSpan');
+    testSpan.innerHTML = m1.testMF();
+}
+
+let testSpan = document.getElementById('testSpan');
+    testSpan.innerHTML = m1.testMF();
